82ebbdcd3e0cad7197b8a740bc21ab0ebe808415
Abdisamed Ahmed <a.s_1985@hotmail.com>
2017-02-15 01:31:06 -0600

    Added main files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>JQuery</title>
+</head>
+<body>
+
+
+<?php  echo "Hello PHP!!!"; ?>
+
+<button id="btn"></button>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+</body>
+</html>
